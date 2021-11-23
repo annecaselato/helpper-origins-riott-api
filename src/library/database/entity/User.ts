434481033169f90b0695dcb9d11a3f99621e8a8b
annecaselato <annecaselato@gmail.com>
2021-11-23 19:04:35 -0300

    Update User.ts

--- src/library/database/entity/User.ts
+++ src/library/database/entity/User.ts
@@ -8,6 +8,12 @@ export class User extends BaseEntity {
     @Column({ unique: true })
     public name: string;
 
+    @Column({ unique: true })
+    public email: string;
+
+    @Column()
+    public password: string;
+
     @Column()
     public createdAt: Date;
 
