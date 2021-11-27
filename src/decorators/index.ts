9cd1a1869c111a3cf625f1247eb415ee36be1e1f
annecaselato <annecaselato@gmail.com>
2021-11-27 09:44:24 -0300

    Remove PrivateRoute decorator

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,6 +1,5 @@
 export * from './Controller';
 export * from './HttpMethods';
 export * from './Middlewares';
-export * from './PrivateRoute';
 export * from './PublicRoute';
 export * from './Singleton';
--- src/models/EnumDecorators.ts
+++ src/models/EnumDecorators.ts
@@ -1,7 +1,6 @@
 export enum EnumDecorators {
     CONTROLLER_PREFIX = 'controller_prefix',
     ROUTES = 'routes',
-    PRIVATE_ROUTES = 'private_routes',
     PUBLIC_ROUTES = 'public_routes',
     MIDDLEWARE = 'middleware'
 }
