9819a4c5bf1b0c3ff73e5e612952f85cb24f7339
annecaselato <annecaselato@gmail.com>
2021-11-27 08:51:44 -0300

    Create PrivateRoute

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,5 +1,6 @@
 export * from './Controller';
 export * from './HttpMethods';
 export * from './Middlewares';
+export * from './PrivateRoute';
 export * from './PublicRoute';
 export * from './Singleton';
--- src/models/EnumDecorators.ts
+++ src/models/EnumDecorators.ts
@@ -1,6 +1,7 @@
 export enum EnumDecorators {
     CONTROLLER_PREFIX = 'controller_prefix',
     ROUTES = 'routes',
+    PRIVATE_ROUTES = 'private_routes',
     PUBLIC_ROUTES = 'public_routes',
     MIDDLEWARE = 'middleware'
 }
