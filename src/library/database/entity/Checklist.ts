0e8a729cff9c09c5371d6de926534cfe55aa993c
annecaselato <annecaselato@gmail.com>
2021-12-03 21:16:31 -0300

    Update Checklist.ts

--- src/library/database/entity/Checklist.ts
+++ src/library/database/entity/Checklist.ts
@@ -1,4 +1,4 @@
-import { Entity, ObjectID, ObjectIdColumn, Column, BeforeInsert, BeforeUpdate, BaseEntity } from 'typeorm';
+import { Entity, ObjectID, ObjectIdColumn, Column, BaseEntity } from 'typeorm';
 
 @Entity()
 export class Checklist extends BaseEntity {
@@ -13,21 +13,4 @@ export class Checklist extends BaseEntity {
 
     @Column()
     public status: string;
-
-    @Column()
-    public createdAt: Date;
-
-    @Column()
-    public updatedAt: Date;
-
-    @BeforeInsert()
-    public setCreateDate(): void {
-        this.createdAt = new Date();
-    }
-
-    @BeforeInsert()
-    @BeforeUpdate()
-    public setUpdateDate(): void {
-        this.updatedAt = new Date();
-    }
 }
