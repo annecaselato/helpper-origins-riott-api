f5cdc24625ce36f810d59496ab91d65aa9292674
annecaselato <annecaselato@gmail.com>
2021-12-03 21:34:01 -0300

    Update index.ts

--- src/library/database/entity/index.ts
+++ src/library/database/entity/index.ts
@@ -2,3 +2,4 @@ export { Application } from './Application';
 export { User } from './User';
 export { Checklist } from './Checklist';
 export { Member } from './Member';
+export { ListItem } from './ListItem';
